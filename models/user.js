419e5b59b1198b138b0f68ed93b9f1d2c1e48220
Kyrylo <fresh.js.ftd.@gmail.com>
2024-03-12 20:41:41 +0200

    Add 1:m model association example

--- models/user.js
+++ models/user.js
@@ -11,6 +11,12 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+
+      User.hasMany(models.Todo, {
+        foreignKey : 'userId',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      });
     }
   }
 
