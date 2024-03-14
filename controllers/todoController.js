99fe24f453578681d5249ffd11ae4f2c949174dc
Kyrylo <fresh.js.ftd.@gmail.com>
2024-03-14 18:32:30 +0200

    Add rest Todo CRUD routes

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -44,3 +44,53 @@ module.exports.getUserTodos = async (req, res, next) => {
     next(error);
   }
 };
+
+module.exports.getTodo = async (req, res, next) => {
+  try {
+    const {
+      params: { todoId, userId },
+    } = req;
+
+    const todo = await Todo.findByPk(todoId);
+
+    res.send({ data: todo });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.updateTodo = async (req, res, next) => {
+  try {
+    const {
+      params: { todoId },
+      body,
+    } = req;
+
+    const [rowsCount, [updatedTodo]] = await Todo.update(body, {
+      where: {
+        id: todoId,
+      },
+      returning: true,
+    });
+
+    res.send({ data: updatedTodo });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.deleteTodo = async (req, res, next) => {
+  try {
+    const {
+      params: { todoId },
+    } = req;
+
+    const todoToDelete = await Todo.findByPk(todoId);
+
+    await todoToDelete.destroy();
+
+    res.send({ data: todoToDelete });
+  } catch (error) {
+    next(error);
+  }
+};
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -13,4 +13,8 @@ userRouter.delete('/:userId', UserController.deleteUser);
 userRouter.post('/:userId/todos', TodoController.createTodo);
 userRouter.get('/:userId/todos', TodoController.getUserTodos);
 
+userRouter.get('/:userId/todos/:todoId', TodoController.getTodo);
+userRouter.put('/:userId/todos/:todoId', TodoController.updateTodo);
+userRouter.delete('/:userId/todos/:todoId', TodoController.deleteTodo);
+
 module.exports = userRouter;
