2fd6ec19b86c6ec0a5b8638a75b17e4a0684600f
Kyrylo <fresh.js.ftd.@gmail.com>
2024-03-14 19:09:40 +0200

    Add todoRouter example

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,8 +1,8 @@
 const userRouter = require('express').Router();
 const UserController = require('../controllers/userController');
-const TodoController = require('../controllers/todoController');
 const paginationMW = require('../middlewares/pagination');
 const { findUser } = require('../middlewares/usersMW');
+const todoRouter = require('./todoRouter');
 
 userRouter.post('/', UserController.createUser);
 userRouter.get('/', paginationMW, UserController.getUsers);
@@ -11,15 +11,6 @@ userRouter.get('/:userId', findUser, UserController.getUser);
 userRouter.put('/:userId', findUser, UserController.updateUser);
 userRouter.delete('/:userId', findUser, UserController.deleteUser);
 
-userRouter.post('/:userId/todos', findUser, TodoController.createTodo);
-userRouter.get('/:userId/todos', findUser, TodoController.getUserTodos);
-
-userRouter.get('/:userId/todos/:todoId', findUser, TodoController.getTodo);
-userRouter.put('/:userId/todos/:todoId', findUser, TodoController.updateTodo);
-userRouter.delete(
-  '/:userId/todos/:todoId',
-  findUser,
-  TodoController.deleteTodo
-);
+userRouter.use('/:userId/todos', findUser, todoRouter);
 
 module.exports = userRouter;
