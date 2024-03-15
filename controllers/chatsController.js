68eb639e2f8d3367c66f936e60ed5ba1df44ce41
Kyrylo <fresh.js.ftd.@gmail.com>
2024-03-15 18:46:09 +0200

    Add eager and lazy loaging examples

--- controllers/chatsController.js
+++ controllers/chatsController.js
@@ -1,3 +1,4 @@
+const { Chat, User } = require('../models');
 const { createChat } = require('../services/chat.service');
 
 module.exports.createChat = async (req, res, next) => {
@@ -16,6 +17,58 @@ module.exports.createChat = async (req, res, next) => {
 
 module.exports.getChats = async (req, res, next) => {
   try {
+    // чати мають повертатися разом з користувачами, які в них знаходяться
+    /*
+      Нетерпляче завантаження - (Eager Loading) - це JOIN      
+    */
+
+    const chats = await Chat.findAll({
+      // LEFT JOIN
+      // include: User,
+      // attributes: [],
+      include: [
+        {
+          model: User,
+          required: true, // INNER JOIN
+          attributes: ['firstName', 'lastName'], // дані з іншої таблиці (User)
+          through: {
+            // керує даними зв'язвальної таблиці
+            attributes: [],
+          },
+        },
+        // {
+        // для під'єдання ще одної моделі
+        //   model: Todo
+        // }
+      ],
+    });
+
+    res.send({ data: chats });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getChat = async (req, res, next) => {
+  try {
+    const {
+      params: { chatId },
+    } = req;
+    // Ліниве завантаження - (Lazy Loading) - це магічні методи
+    const chat = await Chat.findByPk(chatId);
+
+    if (!chat) {
+      throw new Error('Chat not found');
+    }
+
+    const usersInChat = await chat.getUsers();
+
+    res.send({
+      data: {
+        chat,
+        users: usersInChat,
+      },
+    });
   } catch (error) {
     next(error);
   }
--- routers/chatRouter.js
+++ routers/chatRouter.js
@@ -2,5 +2,8 @@ const chatRouter = require('express').Router();
 const ChatConroller = require('../controllers/chatsController');
 
 chatRouter.post('/', ChatConroller.createChat);
+chatRouter.get('/', ChatConroller.getChats);
+
+chatRouter.get('/:chatId', ChatConroller.getChat);
 
 module.exports = chatRouter;
